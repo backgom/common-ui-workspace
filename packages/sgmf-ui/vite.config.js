09cff353addf4a6325b5902659709675faf18d83
Sunghyeon Park <sunghyeon.park@doosan.com>
2022-06-07 16:32:17 +0900

    vite 설정 추가

--- packages/sgmf-ui/vite.config.js
+++ packages/sgmf-ui/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  mode: import.meta.env?.PROD ? 'production' : 'development',
   plugins: [vue()],
   resolve: {
     alias: {
